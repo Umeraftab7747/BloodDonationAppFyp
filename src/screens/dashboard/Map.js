2213b30cee4f4dd220fbbe3c476e02a0cfd91205
Umeraftab7747 <umeraftab7747@gmail.com>
2020-10-31 10:13:41 -0700

    maps

--- src/screens/dashboard/Map.js
+++ src/screens/dashboard/Map.js
@@ -14,27 +14,36 @@ import {Icon} from 'react-native-elements';
 
 // Components
 import {NavHeader} from '../../components';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 export class Map extends Component {
   state = {};
 
   render() {
     return (
-      <View style={styles.Container}>
-        <NavHeader
-          txt="Maps"
-          onPress={() => {
-            this.props.navigation.openDrawer();
-          }}
-        />
+      <View style={styles.container}>
+        <MapView
+          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          style={styles.map}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}></MapView>
       </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
-  Container: {
-    flex: 1,
-    // backgroundColor: Primary,
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
