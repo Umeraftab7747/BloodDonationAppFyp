6142d8d897d78d3e6492876ba4f72ccf976eeba3
Umeraftab7747 <umeraftab7747@gmail.com>
2020-10-31 00:06:07 -0700

    Custom Component add

--- src/screens/dashboard/Request.js
+++ src/screens/dashboard/Request.js
@@ -39,12 +39,44 @@ export class Request extends Component {
             </View>
             <View style={styles.CheckBoxed}>
               {/* checkbox */}
+              <TouchableOpacity
+                style={styles.CheckBoxContainer}
+                onPress={() => {
+                  this.setState({checked: !this.state.checked}, () => {
+                    this.setState({searched: 'Emergency', checked1: false});
+                  });
+                }}>
+                {this.state.checked ? (
+                  <View style={styles.CheckBoxChecked} />
+                ) : (
+                  <View style={styles.CheckBox} />
+                )}
+                <Text>Emergency</Text>
+              </TouchableOpacity>
 
+              {/* Checkbox 2 */}
+              <TouchableOpacity
+                style={styles.CheckBoxContainer}
+                onPress={() => {
+                  this.setState({checked1: !this.state.checked1}, () => {
+                    this.setState({searched: 'Advance', checked: false});
+                  });
+                }}>
+                {this.state.checked1 ? (
+                  <View style={styles.CheckBoxChecked} />
+                ) : (
+                  <View style={styles.CheckBox} />
+                )}
+                <Text>Advance</Text>
+              </TouchableOpacity>
               {/* end CheckBox */}
             </View>
             <View style={styles.Quantity}>
               <Text>Quantity</Text>
-              <TextInput style={styles.QuantityTextField} />
+              <TextInput
+                style={styles.QuantityTextField}
+                placeholder={'4 packets'}
+              />
             </View>
           </View>
           <View style={styles.BottomBox}>
@@ -110,7 +142,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-evenly',
     width: '100%',
     height: h('5%'),
-    // backgroundColor: 'orange',
+    backgroundColor: 'orange',
     alignItems: 'center',
   },
   SelectBloodType: {
@@ -123,11 +155,12 @@ const styles = StyleSheet.create({
   },
   QuantityTextField: {
     backgroundColor: 'white',
-    width: w('30%'),
+    width: '20%',
     color: 'black',
-    height: h('4%'),
+    height: '100%',
 
-    paddingTop: -h('4%'),
+    paddingTop: -h('1%'),
+    paddingBottom: -h('1%'),
   },
   SearchButton: {
     backgroundColor: 'white',
@@ -152,5 +185,23 @@ const styles = StyleSheet.create({
     // justifyContent: 'space-around',
     alignItems: 'center',
     flexDirection: 'row',
+  },
+  CheckBoxContainer: {
+    // backgroundColor: 'red',
+    width: w('30%'),
+    height: h('4%'),
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  CheckBox: {
+    backgroundColor: 'white',
+    width: 20,
+    height: 20,
+  },
+  CheckBoxChecked: {
+    backgroundColor: '#158467',
+    width: 20,
+    height: 20,
   },
 });
